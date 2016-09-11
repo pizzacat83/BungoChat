12f3bcc3fab5c9ec753863043bb2cb954d8d31c9
pizzacat83 <pizzacat83.cpp@gmail.com>
2016-09-11 20:25:06 +0900

    randomresponse.py 追加

---
+++ randomresponse.py
@@ -0,0 +1,5 @@
+﻿src=[s.decode('utf-8') for s in open(r"F:\aozora\kokoro\kokoroserif.txt", "r") if s[0]!="#"]
+from random import choice
+while True:
+	raw_input("user: ")
+	print "soseki:", choice(src).encode('shift-jis')
