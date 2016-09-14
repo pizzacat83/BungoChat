a4227324122fd61427110e00f26a360ac87c78c0
pizzacat83 <pizzacat83.cpp@gmail.com>
2016-09-14 19:55:21 +0900

    main() 独り言モードに変更

--- Respond/main.cpp
+++ Respond/main.cpp
@@ -15,9 +15,18 @@
 int main(){
 	srand((unsigned) time(NULL));
     //mtRand=std::mt19937(devRand());
-    Character * sensei = new Character("F:\\aozora\\kokoro\\kokoro.markov");
+	Character * sensei = new Character(std::string("F:\\Programming\\Desktop\\cpp\\Bungo\\Bungo_2008\\aozora\\kokoro\\kokoro.markov"), std::string("F:\\Programming\\Desktop\\cpp\\Bungo\\Bungo_2008\\aozora\\kokoro\\kokoroserif.txt"));
     //std::cout<<mt.startSentence("下人")<<std::endl;
-	std::cout<<sensei~>Respond("私")<<std::endl;
+	/*while(1){
+		std::string line;
+		std::getline(std::cin, line);
+		std::cout<<sensei->Respond(std::string(line))<<std::endl;
+	}*/
+	std::string line="いやいや";
+	for(int i = 0; i<50; ++i){
+		line=sensei->Respond(line);
+		std::cout<<line<<std::endl;;
+	}
 	delete sensei;
     return 0;
 }
