68bccc5c1d3c086a5329cf69e0a14e93601d38ac
pizzacat83 <pizzacat83.cpp@gmail.com>
2016-09-17 20:19:25 +0900

    main.cpp 対話モード

--- Respond/main.cpp
+++ Respond/main.cpp
@@ -14,20 +14,22 @@
 
 
 int main(){
+	setlocale(LC_CTYPE, "");
 	srand((unsigned) time(NULL));
     //mtRand=std::mt19937(devRand());
 	Character * sensei = new Character(std::string("F:\\Programming\\Desktop\\cpp\\Bungo\\Bungo_2008\\aozora\\kokoro\\kokoro.markov"), std::string("F:\\Programming\\Desktop\\cpp\\Bungo\\Bungo_2008\\aozora\\kokoro\\kokoroserif.txt"));
     //std::cout<<mt.startSentence("下人")<<std::endl;
-	/*while(1){
+	while(1){
 		std::string line;
+		std::cout<<"ぴざきゃっと:　　　";
 		std::getline(std::cin, line);
-		std::cout<<sensei->Respond(std::string(line))<<std::endl;
-	}*/
-	std::string line="いやいや";
+		std::cout<<"しょうもないもの:　"<<sensei->Respond(std::string(line))<<std::endl;
+	}
+	/*std::string line="いやいや";
 	for(int i = 0; i<50; ++i){
 		line=sensei->Respond(line);
 		std::cout<<line<<std::endl;;
-	}
+	}*/
 	delete sensei;
     return 0;
 }
