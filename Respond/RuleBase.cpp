8745b2cce23ed37e5183745a554736d9de05d811
pizzacat83 <pizzacat83.cpp@gmail.com>
2016-09-21 21:17:31 +0900

    replyByWord 質問応対一部実装

--- Respond/RuleBase.cpp
+++ Respond/RuleBase.cpp
@@ -1,4 +1,5 @@
 ﻿#include <vector>
+#include <cmath>
 #include <string>
 #include <fstream>
 #include <random>
@@ -11,6 +12,13 @@ void widen(const std::string &src, std::wstring &dest) {
 	mbstowcs(wcs, src.c_str(), src.length() + 1);
 	dest = wcs;
 	delete [] wcs;
+}
+
+void narrow(const std::wstring &src, std::string &dest) {
+	char *mbs = new char[src.length() * MB_CUR_MAX + 1];
+	wcstombs(mbs, src.c_str(), src.length() * MB_CUR_MAX + 1);
+	dest = mbs;
+	delete [] mbs;
 }
 
 
@@ -32,23 +40,37 @@ int sigtonum(char c){
 void RuleBase::LoadRegex(const std::string& regexpath){
 	std::ifstream ifs(regexpath.c_str());
 	std::string line;
-	char sig, type;
-	while(ifs>>sig>>type,ifs.ignore(),std::getline(ifs,line)){
+	std::string sig, type;
+	std::wstring wline,regsrc;
+	std::tr1::wregex re(L"^(.+)\\t(.+)\\t(.+)$");
+	std::tr1::wsmatch matches;
+	while(/*ifs>>sig>>type,ifs.ignore(),*/std::getline(ifs,line)){
 		if(line.empty())continue;
 		std::wstring wline;
 		widen(line,wline);
-		regexset tmp={sigtonum(sig), type, std::tr1::wregex(wline)};
-		re.push_back(tmp); 
+		if(!std::tr1::regex_match(wline,matches,re))continue;
+		narrow(matches.str(1),sig);
+		narrow(matches.str(2),type);
+		regsrc=matches.str(3);
+		regexset tmp={sigtonum(sig[0]), type[0], std::tr1::wregex(regsrc)};
+		this->re.push_back(tmp); 
     }
 }
 
 
 void RuleBase::LoadSerif(const std::string& serifpath){
 	std::ifstream ifs(serifpath.c_str());
-	std::string sig, line;
-	while(ifs>>sig,ifs.ignore(),std::getline(ifs,line)){
+	std::string sig, serif,line;
+	std::wstring wline;
+	std::tr1::wregex re(L"^(.*)\\t(.+)$");
+	std::tr1::wsmatch matches;
+	while(/*ifs>>sig,ifs.ignore(),*/std::getline(ifs,line)){
+		widen(line,wline);
+		std::tr1::regex_match(wline, matches, re);
+		narrow(matches.str(1), sig);
+		narrow(matches.str(2), serif);
 		for(std::string::iterator it=sig.begin();it!=sig.end(); ++it){
-			serifs[sigtonum(*it)].push_back(line);
+			serifs[sigtonum(*it)].push_back(serif);
 		}
     }
 }
@@ -60,11 +82,13 @@ RuleBase::~RuleBase(void)
 std::string RuleBase::respondRuleBase(const std::string& input, const std::vector<std::string>& words){
 	/*return randomChoice();*/
 	std::string res=replyByWord(input,words);
-	return res!=""?res:randomChoice();
+	if(res!="")return res;
+	if(!(rand()%6))return randomChoice();
+	return "";
 }
 
 std::string RuleBase::randomChoice(){
-	int t = rand()%serifs.size();
+	int t = rand()%serifs[0].size();
 	return serifs[0][t];
 }
 
@@ -93,18 +117,32 @@ std::string RuleBase::replyByWord(const std::string &input, const std::vector<st
 			std::tr1::wsmatch matches;
 			res|=int(std::tr1::regex_search(winput,matches,it->re))<<it->signum;
 			whentype=matches.str(1);
-			if(whentype==L"いつ")whentype=L"";
-			else whentype.substr(1);
+			if(whentype==L""||whentype==L"いつ")whentype=L"";
+			else whentype = whentype.substr(1);
 		}
 	}
-if(res&0x1fc){
-  //質問された
-  if(!serifs[1].empty()){
-    int len=serifs[1].size();
-    int t=rand()%len;
-    return serifs[1][t];
-  }
-}
+	/*if(res&(1<<('i'-'a'))){
+		
+	}*/
+	
+	if(res&0x1fc){
+	//質問された
+		for(int i = 2; i<9; ++i){
+			if(res&(long long(1)<<i)){
+				int len=serifs[i].size();
+				int limit = int(RAND_MAX*atan(float(len)/30)/acos(0.0));
+				if(rand()<limit){
+					//既定の答えを返す
+					return serifs[i][rand()%len];
+				}
+			}
+		}
+		if(!serifs[1].empty()){
+			int len=serifs[1].size();
+			int t=rand()%len;
+			if(!(rand()%6))return serifs[1][t];
+		}
+	}
   
 
 	return "";
